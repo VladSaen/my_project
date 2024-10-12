b97e92a8f27c9f61e75b5f6b0d04acd2a6ffc7f6
Vladyslav <88395639+VladSaen@users.noreply.github.com>
2024-10-12 14:07:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const emailLink = document.querySelector('a.email');
 
 emailLink.addEventListener('click', event => {
   event.preventDefault();
-  mailto('info@freshwash.com');
+  mailto('qwefghjklgfd@gmail.com');
 });
 
 function mailto(address) {
